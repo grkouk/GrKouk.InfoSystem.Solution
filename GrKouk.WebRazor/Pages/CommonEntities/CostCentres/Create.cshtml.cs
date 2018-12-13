7afad217b88410cb4a7ce3725f7fb3ff40829dbe
GrKouk <grkouk@hotmail.com>
2018-12-13 19:25:26 +0200

    Cost Centres

---
+++ GrKouk.WebRazor/Pages/CommonEntities/CostCentres/Create.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using GrKouk.InfoSystem.Domain.Shared;
+
+namespace GrKouk.WebRazor.Pages.CommonEntities.CostCentres
+{
+    public class CreateModel : PageModel
+    {
+        private readonly GrKouk.WebApi.Data.ApiDbContext _context;
+
+        public CreateModel(GrKouk.WebApi.Data.ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public CostCentre CostCentre { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.CostCentres.Add(CostCentre);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ GrKouk.WebRazor/Pages/CommonEntities/CostCentres/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GrKouk.InfoSystem.Domain.Shared;
+using GrKouk.WebApi.Data;
+
+namespace GrKouk.WebRazor.Pages.CommonEntities.CostCentres
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly GrKouk.WebApi.Data.ApiDbContext _context;
+
+        public DetailsModel(GrKouk.WebApi.Data.ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        public CostCentre CostCentre { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            CostCentre = await _context.CostCentres.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (CostCentre == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
