fc474101583ded22909ecf098a939b059fa751fc
GrKouk <grkouk@hotmail.com>
2018-12-14 10:15:45 +0200

    Minnor changes in views

---
+++ GrKouk.WebRazor/Pages/CommonEntities/FinCategories/Edit.cshtml.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GrKouk.InfoSystem.Domain.Shared;
+
+namespace GrKouk.WebRazor.Pages.CommonEntities.FinCategories
+{
+    public class EditModel : PageModel
+    {
+        private readonly GrKouk.WebApi.Data.ApiDbContext _context;
+
+        public EditModel(GrKouk.WebApi.Data.ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public FinTransCategory FinTransCategory { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            FinTransCategory = await _context.FinTransCategories.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (FinTransCategory == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(FinTransCategory).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FinTransCategoryExists(FinTransCategory.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool FinTransCategoryExists(int id)
+        {
+            return _context.FinTransCategories.Any(e => e.Id == id);
+        }
+    }
+}
