8be381fe228c20d5ae6fd3c794d7f2ad4debe874
GrKouk <grkouk@hotmail.com>
2019-11-18 11:10:08 +0200

    Added Original filename for media

---
+++ GrKouk.WebApi/Migrations/20191118085257_MediaOriginalFilename.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GrKouk.WebApi.Migrations
+{
+    public partial class MediaOriginalFilename : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OriginalFileName",
+                table: "MediaEntries",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OriginalFileName",
+                table: "MediaEntries");
+        }
+    }
+}
--- GrKouk.WebRazor/Pages/MediaMng/Index.cshtml.cs
+++ GrKouk.WebRazor/Pages/MediaMng/Index.cshtml.cs
@@ -34,38 +34,11 @@ namespace GrKouk.WebRazor.Pages.MediaMng
         }
         private void LoadFilters()
         {
-            //var materialNatures = FiltersHelper.GetWarehouseItemNaturesList();
-            //ViewData["MaterialNatureValues"] = new SelectList(materialNatures, "Value", "Text");
+          
 
             var pageFilterSize = PageFilter.GetPageSizeFiltersSelectList();
             ViewData["PageFilterSize"] = new SelectList(pageFilterSize, "Value", "Text");
 
-            //var dbCompanies = _context.Companies.OrderBy(p => p.Code).AsNoTracking();
-            //List<SelectListItem> companiesList = new List<SelectListItem>();
-            //companiesList.Add(new SelectListItem() { Value = 0.ToString(), Text = "{All Companies}" });
-            //foreach (var company in dbCompanies)
-            //{
-            //    companiesList.Add(new SelectListItem() { Value = company.Id.ToString(), Text = company.Code });
-            //}
-            //ViewData["CompanyFilter"] = new SelectList(companiesList, "Value", "Text");
-
-            //var dbClientProfiles = _context.ClientProfiles.OrderBy(p => p.Name).AsNoTracking();
-            //List<SelectListItem> clientProfilesList = new List<SelectListItem>();
-            //clientProfilesList.Add(new SelectListItem() { Value = 0.ToString(), Text = "{All Profiles}" });
-            //foreach (var clientProfile in dbClientProfiles)
-            //{
-            //    clientProfilesList.Add(new SelectListItem() { Value = clientProfile.Id.ToString(), Text = clientProfile.Name });
-            //}
-            //ViewData["ClientProfileFilter"] = new SelectList(clientProfilesList, "Value", "Text");
-
-            //var dbCashRegCategories = _context.CashRegCategories.OrderBy(p => p.Name).AsNoTracking();
-            //List<SelectListItem> cashRegRegCateroryList = new List<SelectListItem>();
-            //cashRegRegCateroryList.Add(new SelectListItem() { Value = 0.ToString(), Text = "{All Categories}" });
-            //foreach (var cashRegCategory in dbCashRegCategories)
-            //{
-            //    cashRegRegCateroryList.Add(new SelectListItem() { Value = cashRegCategory.Id.ToString(), Text = cashRegCategory.Name });
-            //}
-            //ViewData["CashRegCategoryFilter"] = new SelectList(cashRegRegCateroryList, "Value", "Text");
         }
     }
 }
