777d04773fa082981a827553ea652528471c91de
GrKouk <grkouk@hotmail.com>
2019-12-17 11:44:17 +0200

    Display attributes in Dto

--- GrKouk.InfoSystem.Dtos/WebDtos/BuyDocuments/BuyDocCreateDto.cs
+++ GrKouk.InfoSystem.Dtos/WebDtos/BuyDocuments/BuyDocCreateDto.cs
@@ -12,19 +12,20 @@ namespace GrKouk.InfoSystem.Dtos.WebDtos.BuyDocuments
         public int Id { get; set; }
 
         [DataType(DataType.Date)]
+        [Display(Name = "Trans Date")]
         public DateTime TransDate { get; set; }
-       
+        [Display(Name = "Ref Code")]
         public string TransRefCode { get; set; }
-
+        [Display(Name = "Section")]
         public int SectionId { get; set; }
         public string  SectionCode { get; set; }
-
+        [Display(Name = "Transactor")]
         public int TransactorId { get; set; }
         public  string TransactorName { get; set; }
-
+        [Display(Name = "Fiscal Period")]
         public int FiscalPeriodId { get; set; }
         public  string FiscalPeriodCode { get; set; }
-
+        [Display(Name = "Doc Series")]
         public int BuyDocSeriesId { get; set; }
         public  string BuyDocSeriesCode { get; set; }
         public string BuyDocSeriesName { get; set; }
@@ -32,15 +33,19 @@ namespace GrKouk.InfoSystem.Dtos.WebDtos.BuyDocuments
         public int BuyDocTypeId { get; set; }
         public  string BuyDocTypeCode { get; set; }
         public string BuyDocTypeName { get; set; }
-
+        [Display(Name = "VAT Amount")]
         public decimal AmountFpa { get; set; }
+        [Display(Name = "Net Amount")]
         public decimal AmountNet { get; set; }
+        [Display(Name = "Discount Amount")]
         public decimal AmountDiscount { get; set; }
 
         [MaxLength(500)]
         public string Etiology { get; set; }
+        [Display(Name = "Company")]
         public int CompanyId { get; set; }
         public string CompanyCode { get; set; }
+        [Display(Name = "Payment Method")]
         public int PaymentMethodId { get; set; }
 
         [Timestamp]
