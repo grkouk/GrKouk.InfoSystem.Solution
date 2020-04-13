02a94a08a67ca152f5b31331b47ac673f5496d39
GrKouk <grkouk@hotmail.com>
2020-04-13 20:51:28 +0300

    Recurring Continued

--- GrKouk.WebRazor/Pages/Transactions/BuyMaterialsDoc/Delete.cshtml.cs
+++ GrKouk.WebRazor/Pages/Transactions/BuyMaterialsDoc/Delete.cshtml.cs
@@ -51,28 +51,28 @@ namespace GrKouk.WebRazor.Pages.Transactions.BuyMaterialsDoc
             {
                 return NotFound();
             }
-            #region Section Management
-
-            var section = await _context.Sections.SingleOrDefaultAsync(s => s.SystemName == sectionCode);
-            if (section == null)
-            {
-               
-               
-                return NotFound(new
-                {
-                    error = "Could not locate section "
-                });
-            }
-
-            #endregion
+            // #region Section Management
+            //
+            // var section = await _context.Sections.SingleOrDefaultAsync(s => s.SystemName == sectionCode);
+            // if (section == null)
+            // {
+            //    
+            //    
+            //     return NotFound(new
+            //     {
+            //         error = "Could not locate section "
+            //     });
+            // }
+            //
+            // #endregion
             BuyDocument = await _context.BuyDocuments.FindAsync(id);
 
             if (BuyDocument != null)
             {
+                
                 _context.BuyDocLines.RemoveRange(_context.BuyDocLines.Where(p => p.BuyDocumentId == id));
-              //  _context.SupplierTransactions.RemoveRange(_context.SupplierTransactions.Where(p => p.SectionId == section.Id && p.CreatorId == id));
-                _context.TransactorTransactions.RemoveRange(_context.TransactorTransactions.Where(p => p.SectionId == section.Id && p.CreatorId == id));
-                _context.WarehouseTransactions.RemoveRange(_context.WarehouseTransactions.Where(p => p.SectionId == section.Id && p.CreatorId == id));
+                _context.TransactorTransactions.RemoveRange(_context.TransactorTransactions.Where(p => p.SectionId ==  BuyDocument.SectionId && p.CreatorId == id));
+                _context.WarehouseTransactions.RemoveRange(_context.WarehouseTransactions.Where(p => p.SectionId ==  BuyDocument.SectionId && p.CreatorId == id));
 
                 _context.BuyDocuments.Remove(BuyDocument);
 
---
+++ GrKouk.WebRazor/Pages/Transactions/RecurringTransactions/Delete.cshtml.cs
@@ -0,0 +1,74 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using GrKouk.InfoSystem.Domain.RecurringTransactions;
+using GrKouk.InfoSystem.Domain.Shared;
+using GrKouk.WebApi.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace GrKouk.WebRazor.Pages.Transactions.RecurringTransactions
+{
+    public class Delete : PageModel
+    {
+        private readonly ApiDbContext _context;
+
+        public Delete(ApiDbContext context)
+        {
+            _context = context;
+        }
+        [BindProperty]
+        public RecurringTransDoc ItemVm { get; set; }
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            ItemVm = await _context.RecurringTransDocs
+                .Include(b => b.Company)
+                .Include(b => b.Transactor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ItemVm == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            //const string sectionCode = "SYS-BUY-MATERIALS-SCN";
+            if (id == null)
+            {
+                return NotFound();
+            }
+            // #region Section Management
+            //
+            // var section = await _context.Sections.SingleOrDefaultAsync(s => s.SystemName == sectionCode);
+            // if (section == null)
+            // {
+            //    
+            //    
+            //     return NotFound(new
+            //     {
+            //         error = "Could not locate section "
+            //     });
+            // }
+            //
+            // #endregion
+            ItemVm = await _context.RecurringTransDocs.FindAsync(id);
+
+            if (ItemVm != null)
+            {
+               
+                _context.RecurringTransDocLines.RemoveRange(_context.RecurringTransDocLines.Where(p => p.RecurringTransDocId == id));
+               
+                _context.RecurringTransDocs.Remove(ItemVm);
+
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
