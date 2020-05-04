44b004cd2b9672e5b85fd534ee7ff5f34ca86eef
grkoukrider <grkouk1966@gmail.com>
2020-05-04 17:21:18 +0300

    Details phase 1

---
+++ GrKouk.InfoSystem.Dtos/WebDtos/Transactors/TransactorDetailDto.cs
@@ -0,0 +1,12 @@
+﻿namespace GrKouk.InfoSystem.Dtos.WebDtos.Transactors
+{
+    public class TransactorDetailDto
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string TransactorTypeName { get; set; }
+        public string TransactorTypeCode { get; set; }
+        public string Companies { get; set; }
+    }
+}
